a0c0ad8dbf051eaf99a0271cb2b9df58bed866fa
Jaron Devaraj <jaronelwin@gmail.com>
2025-11-22 09:29:08 +0530

    Add Student.py to display student details

---
+++ Student.py
@@ -0,0 +1,15 @@
+import sys
+
+if len(sys.argv) ! = 3:
+     print("Usage: Python student.py <name>")
+     sys.exit(1)
+
+#sys.argv[0] is always the program name
+script_name = sys.argv[0]
+name = sys.argv[1]
+rollno = sys.argv[2]
+
+print("Script Name:", script_name)
+print("Student Name:", name)
+print("Roll Number:", rollno)
+
